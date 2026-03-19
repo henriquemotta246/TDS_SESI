051d00ca1f3b8c020265d57075826b1fbc4f910c
henriquemotta246 <henriquefioresemotta@gmail.com>
2026-03-19 12:18:53 -0300

    Funções em js

---
+++ controle/switch.js
@@ -0,0 +1,39 @@
+let dia = 5;
+switch (dia) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-feira");
+        break;
+    case 3:
+        console.log("Terça-feira");
+        break;
+    case 4:
+        console.log("Quarta-feira");
+        break
+    case 5:
+        console.log("Quinta-feira");
+        break;
+    case 6:
+        console.log("Sexta-feira");
+        break;
+    case 7:
+        console.log("Sabado");
+        break;
+
+    default:
+        console.log("Numero invalida");
+        break;
+}
+        console.log("Dia");
+
+
+
+   template_string.js
+
+
+let idade = 16 ;
+// imprimir no terminal o nome e a idade junto com uma frase.
+console.log(`Meu nome é ${nome}  e tenho  ${idade} anos de idade.`);
+let nome = "Joao Zuchelli";
---
+++ controle/while.js
@@ -0,0 +1,6 @@
+let contador = 0 ;
+        while (contador < 10) {
+        contador++;
+        console.log("Seu valor: " + contador);
+    }
+    console.log("Fim");
