4ff35a1a41ba8e1dec1d6f74bc17a30b044e91e6
henriquemotta24 <henriqueioresemotta@gmail.com>
2026-03-27 11:06:22 -0300

    atividades

---
+++ desafios/atividade3.js
@@ -0,0 +1,7 @@
+var idade = 15;
+var convite = true;
+if(idade >= 18 &&possuiConvite == true) {
+    console.log("Voce pode entrar");
+}else{(idade <=17)
+    console.log("Voce nao pode entrar");
+}
